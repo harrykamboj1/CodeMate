d6964088b9749e23e768b8d8e5de61718bb7cd60
harrykamboj1 <singhharnoor116@gmail.com>
2024-07-14 17:39:06 +0530

    Minor change

--- pages/api/message.ts
+++ pages/api/message.ts
@@ -1,29 +1,29 @@
-import type { NextApiRequest, NextApiResponse } from "next";
-import { db } from "@/db";
-import { messages, messageSchema, room } from "@/db/schema";
-import { eq } from "drizzle-orm";
+// import type { NextApiRequest, NextApiResponse } from "next";
+// import { db } from "@/db";
+// import { messages, messageSchema, room } from "@/db/schema";
+// import { eq } from "drizzle-orm";
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  if (req.method === "POST") {
-    const { roomId, userId, content } = req.body;
-    const [message] = await db
-      .insert(messages)
-      .values({ roomId, userId, message: content })
-      .returning()
-      .execute();
-    res.status(201).json(message);
-  } else if (req.method === "GET") {
-    const { roomId } = req.query;
-    const prevmessages = await db
-      .select()
-      .from(messages)
-      .where(eq(messages.roomId, roomId))
-      .execute();
-    res.status(200).json(prevmessages);
-  } else {
-    res.status(405).end();
-  }
-}
+// export default async function handler(
+//   req: NextApiRequest,
+//   res: NextApiResponse
+// ) {
+//   if (req.method === "POST") {
+//     const { roomId, userId, content } = req.body;
+//     const [message] = await db
+//       .insert(messages)
+//       .values({ roomId, userId, message: content })
+//       .returning()
+//       .execute();
+//     res.status(201).json(message);
+//   } else if (req.method === "GET") {
+//     const { roomId } = req.query;
+//     const prevmessages = await db
+//       .select()
+//       .from(messages)
+//       .where(eq(messages.roomId, roomId!))
+//       .execute();
+//     res.status(200).json(prevmessages);
+//   } else {
+//     res.status(405).end();
+//   }
+// }
